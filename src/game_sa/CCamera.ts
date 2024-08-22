f5147ec09ae5dc1618d17fd4ff30b3223e9e2fef
vitordvr <codevitor3g@gmail.com>
2024-08-22 03:54:38 -0300

    pre-save

--- src/game_sa/CCamera.ts
+++ src/game_sa/CCamera.ts
@@ -10,18 +10,18 @@ class CCamera {
       this.g_core.getRenderer().renderer.domElement
     );
 
-    this.camera.enableRotate = true;
-    this.camera.enablePan = true;
-    this.camera.enableZoom = true;
-    this.camera.maxPolarAngle = Math.PI / 2.1;
-    this.camera.minPolarAngle = Math.PI / 4;
-    this.camera.target.set(0, 1, 0);
-    this.camera.update();
+    // this.camera.enableRotate = true;
+    // this.camera.enablePan = true;
+    // this.camera.enableZoom = true;
+    // this.camera.maxPolarAngle = Math.PI / 2.1;
+    // this.camera.minPolarAngle = Math.PI / 4;
+    // this.camera.target.set(0, 1, 0);
+    // this.camera.update();
 
-    this.camera.minDistance = 3;
-    this.camera.maxDistance = 3;
+    // this.camera.minDistance = 3;
+    // this.camera.maxDistance = 3;
 
-    this.update();
+    // this.update();
   }
 
   public update() {
